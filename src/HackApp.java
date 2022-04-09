e2ea5006b35a8fd8673c0b55da1019aad8f66ea2
Harsh Gandhi <harshgandhi@harshs-mbp.quinnipiac.edu>
2022-04-09 09:45:01 -0400

    added title

--- src/HackApp.java
+++ src/HackApp.java
@@ -1,4 +1,7 @@
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class HackApp {
 
@@ -7,8 +10,11 @@ public class HackApp {
 		JFrame frame = new JFrame("Accessability App");
 		frame.setSize(800, 600);
 		frame.setVisible(true);
-		
-		
+		JLabel entry = new JLabel("Welcome to our app");
+		entry.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		frame.add(entry);
+		entry.setSize(200, 60);
+		entry.setLocation(325,150);
 	}
 
 }
