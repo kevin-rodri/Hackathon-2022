7eb190de8fba1dc120e5fb730cce9770f6c19ff0
kevin-rodri <kr399880@gmail.com>
2022-04-09 09:28:29 -0400

    initial commit

---
+++ src/HackApp.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class HackApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Accessability App");
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		
+		
+	}
+
+}
